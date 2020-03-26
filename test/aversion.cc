440693d0abc6cd76bb963a85fe2f1f856757fd90
Force Charlie <charlieio@outlook.com>
2020-03-26 13:14:25 +0800

    baulk::version::version fix now support parse 1.0-rc.1

---
+++ test/aversion.cc
@@ -0,0 +1,27 @@
+#include <string_view>
+#include <cstdio>
+#include <vector>
+#include "../include/version.hpp"
+
+int main() {
+  const std::string_view vs[] = {"19.00",
+                                 "2020-02-25/2.2.0-24-gb93c271",
+                                 "3.2",
+                                 "1.35.0",
+                                 "v0.13.0",
+                                 "2.091.0",
+                                 "v8.1.0.0p1-Beta",
+                                 "0.49",
+                                 "v7.5.0",
+                                 "10.0.19041.153",
+                                 "7.69.1",
+                                 "1.0-rc.1",
+                                 "1.2.3-rc.4"};
+  for (auto v : vs) {
+    baulk::version::version sv(v);
+    auto str = sv.to_string();
+    fprintf(stderr, "resolve version(%u.%u.%u.%u): %s to %s\n", sv.major,
+            sv.minor, sv.patch, sv.build, v.data(), str.data());
+  }
+  return 0;
+}
