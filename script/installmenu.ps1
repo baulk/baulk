add2ab5095a4d0b8fd5d1de150f943f7b56d4ab1
Force.Charlie <charlieio@outlook.com>
2020-05-23 11:34:42 +0800

    Add script to support initialize explorer menu item

---
+++ script/installmenu.ps1
@@ -0,0 +1,19 @@
+#!/usr/bin/env pwsh
+$BaulkRoot = Split-Path -Parent $PSScriptRoot
+$BaulkTerminal = Join-Path -Path $BaulkRoot -ChildPath "baulkterminal.exe"
+
+if (!(Test-Path $BaulkTerminal)) {
+    Write-Host -ForegroundColor Red "$BaulkTerminal not exists"
+}
+
+New-Item -Path 'Registry::HKEY_CLASSES_ROOT\Directory\shell\baulk' -Force | Out-Null
+New-ItemProperty -Path 'Registry::HKEY_CLASSES_ROOT\Directory\shell\baulk' -Name 'MUIVerb' -PropertyType String -Value 'Baulk Terminal here' | Out-Null
+New-ItemProperty -Path 'Registry::HKEY_CLASSES_ROOT\Directory\shell\baulk' -Name 'Icon' -PropertyType String -Value $BaulkTerminal | Out-Null
+New-Item -Path 'Registry::HKEY_CLASSES_ROOT\Directory\shell\baulk\command' -Force | Out-Null
+New-ItemProperty -Path 'Registry::HKEY_CLASSES_ROOT\Directory\shell\baulk\command' -Name '(Default)' -PropertyType String -Value "`"$BaulkTerminal`" --vs" | Out-Null
+
+New-Item -Path 'Registry::HKEY_CLASSES_ROOT\Directory\Background\shell\baulk' -Force | Out-Null
+New-ItemProperty -Path 'Registry::HKEY_CLASSES_ROOT\Directory\Background\shell\baulk' -Name 'MUIVerb' -PropertyType String -Value 'Baulk Terminal here' | Out-Null
+New-ItemProperty -Path 'Registry::HKEY_CLASSES_ROOT\Directory\Background\shell\baulk' -Name 'Icon' -PropertyType String -Value $BaulkTerminal | Out-Null
+New-Item -Path 'Registry::HKEY_CLASSES_ROOT\Directory\Background\shell\baulk\command' -Force | Out-Null
+New-ItemProperty -Path 'Registry::HKEY_CLASSES_ROOT\Directory\Background\shell\baulk\command' -Name '(Default)' -PropertyType String -Value "`"$BaulkTerminal`" --vs" | Out-Null
