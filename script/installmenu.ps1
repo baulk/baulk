2d6dad91847c8abac753bd31ee8152a86fb3a240
Force.Charlie <charlieio@outlook.com>
2020-05-26 07:15:28 +0800

    baulk Imporve Explorer context menu cwd

--- script/installmenu.ps1
+++ script/installmenu.ps1
@@ -10,10 +10,10 @@ New-Item -Path 'Registry::HKEY_CLASSES_ROOT\Directory\shell\baulk' -Force | Out-
 New-ItemProperty -Path 'Registry::HKEY_CLASSES_ROOT\Directory\shell\baulk' -Name 'MUIVerb' -PropertyType String -Value 'Baulk Terminal here' | Out-Null
 New-ItemProperty -Path 'Registry::HKEY_CLASSES_ROOT\Directory\shell\baulk' -Name 'Icon' -PropertyType String -Value $BaulkTerminal | Out-Null
 New-Item -Path 'Registry::HKEY_CLASSES_ROOT\Directory\shell\baulk\command' -Force | Out-Null
-New-ItemProperty -Path 'Registry::HKEY_CLASSES_ROOT\Directory\shell\baulk\command' -Name '(Default)' -PropertyType String -Value "`"$BaulkTerminal`" --vs" | Out-Null
+New-ItemProperty -Path 'Registry::HKEY_CLASSES_ROOT\Directory\shell\baulk\command' -Name '(Default)' -PropertyType String -Value "`"$BaulkTerminal`" --vs -W ""%V.""" | Out-Null
 
 New-Item -Path 'Registry::HKEY_CLASSES_ROOT\Directory\Background\shell\baulk' -Force | Out-Null
 New-ItemProperty -Path 'Registry::HKEY_CLASSES_ROOT\Directory\Background\shell\baulk' -Name 'MUIVerb' -PropertyType String -Value 'Baulk Terminal here' | Out-Null
 New-ItemProperty -Path 'Registry::HKEY_CLASSES_ROOT\Directory\Background\shell\baulk' -Name 'Icon' -PropertyType String -Value $BaulkTerminal | Out-Null
 New-Item -Path 'Registry::HKEY_CLASSES_ROOT\Directory\Background\shell\baulk\command' -Force | Out-Null
-New-ItemProperty -Path 'Registry::HKEY_CLASSES_ROOT\Directory\Background\shell\baulk\command' -Name '(Default)' -PropertyType String -Value "`"$BaulkTerminal`" --vs" | Out-Null
+New-ItemProperty -Path 'Registry::HKEY_CLASSES_ROOT\Directory\Background\shell\baulk\command' -Name '(Default)' -PropertyType String -Value "`"$BaulkTerminal`" --vs -W ""%V.""" | Out-Null
