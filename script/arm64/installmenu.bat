75cb26a98341a55f37ab0ae174ad33cc1fd05e77
Force Charlie <charlieio@outlook.com>
2020-09-25 12:43:25 +0800

    baulk menu script detect pwsh exists in path

--- script/arm64/installmenu.bat
+++ script/arm64/installmenu.bat
@@ -1,2 +1,8 @@
 @echo off
-pwsh -NoProfile -NoLogo -ExecutionPolicy unrestricted -File "%~dp0installmenu.ps1" %*
+where pwsh.exe >nul 2>nul
+if %ERRORLEVEL% NEQ 0 (
+    echo It is recommended to install PowerShell 7^+ for a better experience.
+    powershell -NoProfile -NoLogo -ExecutionPolicy unrestricted -File "%~dp0installmenu.ps1" %*
+) else (
+    pwsh -NoProfile -NoLogo -ExecutionPolicy unrestricted -File "%~dp0installmenu.ps1" %*
+)
