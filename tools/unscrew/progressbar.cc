a121bec91aac749301076302ddec0ab2ecae9b51
Force Charlie <charlieio@outlook.com>
2021-10-28 23:37:25 +0800

    update code

--- tools/unscrew/progressbar.cc
+++ tools/unscrew/progressbar.cc
@@ -11,6 +11,7 @@
 #include "progressbar.hpp"
 
 namespace baulk::unscrew {
+static std::atomic_bool canceled{false};
 HRESULT WINAPI taskdialog_callback_impl(__in HWND hWnd, __in UINT msg, __in WPARAM wParam, __in LPARAM lParam,
                                         __in LONG_PTR lpRefData) {
   UNREFERENCED_PARAMETER(lpRefData);
@@ -22,23 +23,35 @@ HRESULT WINAPI taskdialog_callback_impl(__in HWND hWnd, __in UINT msg, __in WPAR
     // SendMessageW(hWnd, TDM_SET_PROGRESS_BAR_RANGE, MAKEWPARAM(0, 100), 0);
     //  https://docs.microsoft.com/en-us/windows/win32/controls/tdm-set-progress-bar-range
     break;
+  case TDN_DESTROYED:
+
+    break;
   case TDN_RADIO_BUTTON_CLICKED:
     break;
   case TDN_BUTTON_CLICKED:
-    // TDM_SET_PROGRESS_BAR_MARQUEE
+    if (IDCANCEL == wParam) {
+      canceled = true;
+      SendMessageW(hWnd, TDM_SET_PROGRESS_BAR_STATE, PBST_PAUSED, 0);
+      return S_FALSE;
+    }
+
+    // MessageBoxW(hWnd, L"destroyed", L"destroyed", MB_OK);
+    //  TDM_SET_PROGRESS_BAR_MARQUEE
     break;
   case TDN_TIMER: {
-    // wParam is the number of milliseconds since the dialog was created or this notification returned TRUE
-    int dwTickCount = (int)wParam / 200;
-    auto msg = bela::StringCat(L"Extract file-", dwTickCount);
-    SendMessageW(hWnd, TDM_UPDATE_ELEMENT_TEXT, TDE_CONTENT, (LPARAM)msg.data());
-    if (dwTickCount < 100) {
-      SendMessageW(hWnd, TDM_SET_PROGRESS_BAR_POS, dwTickCount, 0);
-    } else if (dwTickCount < 130) {
-      SendMessageW(hWnd, TDM_SET_PROGRESS_BAR_POS, 100, 0);
-    } else {
-      // Return S_FALSE to reset dwTickCount
-      hr = TRUE;
+    if (!canceled) {
+      // wParam is the number of milliseconds since the dialog was created or this notification returned TRUE
+      int dwTickCount = (int)wParam / 200;
+      auto msg = bela::StringCat(L"Extract file-", dwTickCount);
+      SendMessageW(hWnd, TDM_UPDATE_ELEMENT_TEXT, TDE_CONTENT, (LPARAM)msg.data());
+      if (dwTickCount < 100) {
+        SendMessageW(hWnd, TDM_SET_PROGRESS_BAR_POS, dwTickCount, 0);
+      } else if (dwTickCount < 130) {
+        SendMessageW(hWnd, TDM_SET_PROGRESS_BAR_POS, 100, 0);
+      } else {
+        // Return S_FALSE to reset dwTickCount
+        hr = TRUE;
+      }
     }
   }
 
@@ -57,7 +70,7 @@ bool UnscrewMessageBox(HWND hWnd, LPCWSTR pszWindowTitle, LPCWSTR pszContent) {
   int nRadioButton = 0;
   tc.cbSize = sizeof(tc);
   tc.hwndParent = hWnd;
-  tc.dwCommonButtons = TDCBF_CANCEL_BUTTON;
+  tc.dwCommonButtons = TDCBF_CANCEL_BUTTON | TDCBF_CLOSE_BUTTON;
   tc.hInstance = GetModuleHandleW(nullptr);
   tc.pszWindowTitle = pszWindowTitle;
   tc.pszMainInstruction = L"---";
@@ -66,8 +79,6 @@ bool UnscrewMessageBox(HWND hWnd, LPCWSTR pszWindowTitle, LPCWSTR pszContent) {
   tc.dwFlags = TDF_ALLOW_DIALOG_CANCELLATION | TDF_POSITION_RELATIVE_TO_WINDOW | TDF_SIZE_TO_CONTENT |
                TDF_SHOW_PROGRESS_BAR | TDF_CALLBACK_TIMER;
   tc.nDefaultRadioButton = nRadioButton;
-  tc.pszCollapsedControlText = L"More information";
-  tc.pszExpandedControlText = L"Less information";
   tc.pfCallback = taskdialog_callback_impl;
   if (hWnd) {
     if (auto hIcon = reinterpret_cast<HICON>(SendMessageW(hWnd, WM_GETICON, ICON_BIG, 0)); hIcon) {
