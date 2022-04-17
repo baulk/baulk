a16c322cbceca1ec8ce400d12c2f960aac81097b
Force Charlie <charlieio@outlook.com>
2022-04-17 21:17:33 +0800

    update baulk extract usage

--- tools/baulk/commands.extract.cc
+++ tools/baulk/commands.extract.cc
@@ -8,13 +8,21 @@ namespace baulk::commands {
 
 void usage_extract() {
   bela::FPrintF(stderr, LR"(Usage: baulk extract [archive] [destination]
-Extract files in a tar archive. support: tar.xz tar.bz2 tar.gz tar.zstd
+Extract files from archive. alias 'e'.
+
+Supported formats:
+  zip (family) archive, supported methods: deflate, deflate64, zstd, bzip2, xz, lzma, Ppmd.
+  tar, tar.gz, tar.xz, tar.bz2, tar.xz, tar.zstd
+  gz, xz, bzip2, zstd,
+  msi
+  self-extracting archive
+  7z supported archive
 
 Example:
   baulk extract curl-7.80.0.tar.gz
   baulk extract curl-7.80.0.tar.gz curl-dest
-  baulk e curl-7.80.0.tar.gz
-  baulk e curl-7.80.0.tar.gz curl-dest
+  baulk e curl-7.80.0.zip
+  baulk e curl-7.80.0.zip curl-dest
 
 )");
 }
