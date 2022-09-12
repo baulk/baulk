96ec6e0c1f5776b33fc1473e1b5aae525f50abd1
Force Charlie <charlieio@outlook.com>
2022-09-12 20:57:43 +0800

    echo code update

--- include/baulk/echo.hpp
+++ include/baulk/echo.hpp
@@ -11,6 +11,9 @@ namespace baulk::echo {
 constexpr uint8_t packet_end[4] = {'0', '0', '0', '0'};
 constexpr std::int64_t packet_max_len = (65536ll - std::size(packet_end));
 
+constexpr std::string_view commands[] = {"install", "uninstall", "search", "list",   "version", "info",
+                                         "update",  "upgrade",   "freeze", "extact", "bucket"};
+
 // Writer echo writer
 class Writer {
 public:
