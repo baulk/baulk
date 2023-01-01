0b760d01a1795e6c0834cacf86e9ebec59c54cba
Force Charlie <charlieio@outlook.com>
2023-01-01 22:34:49 +0800

    zip: update extraID

--- lib/archive/zip/zipinternal.hpp
+++ lib/archive/zip/zipinternal.hpp
@@ -51,18 +51,59 @@ constexpr auto uint32max = (std::numeric_limits<uint32_t>::max)();
 // have been invented. Pervasive use effectively makes them "official".
 //
 // See http://mdfs.net/Docs/Comp/Archiving/Zip/ExtraField
-constexpr int zip64ExtraID = 0x0001;                 // Zip64 extended information
-constexpr int ntfsExtraID = 0x000a;                  // NTFS
-constexpr int unixExtraID = 0x000d;                  // UNIX
-constexpr int extTimeExtraID = 0x5455;               // Extended timestamp
-constexpr int infoZipUnixExtraID = 0x5855;           // Info-ZIP Unix extension
-constexpr int experimentalXlID = 0x6c78;             // Experimental 'xl' field
-constexpr int infoZipUnicodeCommentExtraID = 0x6375; // Info-ZIP Unicode Comment Extra Field.
-constexpr int infoZipUnicodePathID = 0x7075;         // Info-ZIP Unicode Path Extra Field.
-constexpr int infoZipUnixExtra2ID = 0x7855;          // Info-ZIP Unix Extra Field (type 2) "Ux". *
-constexpr int infoZipUnixExtra3ID = 0x7875;          // Info-Zip Unix Extra Field (type 3) "ux". *
-constexpr int winzipAesExtraID = 0x9901;             // winzip AES Extra Field
-constexpr int apkAlignExtraID = 0xD935;              // ApkAlign
+
+// 0x07c8        Macintosh
+// 0x1986        Pixar USD header ID
+// 0x2605        ZipIt Macintosh
+// 0x2705        ZipIt Macintosh 1.3.5+
+// 0x2805        ZipIt Macintosh 1.3.5+
+// 0x334d        Info-ZIP Macintosh
+// 0x4154        Tandem
+// 0x4341        Acorn/SparkFS
+// 0x4453        Windows NT security descriptor (binary ACL)
+// 0x4704        VM/CMS
+// 0x470f        MVS
+// 0x4854        THEOS (old?)
+// 0x4b46        FWKCS MD5 (see below)
+// 0x4c41        OS/2 access control list (text ACL)
+// 0x4d49        Info-ZIP OpenVMS
+// 0x4d63        Macintosh Smartzip (??)
+// 0x4f4c        Xceed original location extra field
+// 0x5356        AOS/VS (ACL)
+// 0x5455        extended timestamp
+// 0x554e        Xceed unicode extra field
+// 0x5855        Info-ZIP UNIX (original, also OS/2, NT, etc)
+// 0x6375        Info-ZIP Unicode Comment Extra Field
+// 0x6542        BeOS/BeBox
+// 0x6854        THEOS
+// 0x7075        Info-ZIP Unicode Path Extra Field
+// 0x7441        AtheOS/Syllable
+// 0x756e        ASi UNIX
+// 0x7855        Info-ZIP UNIX (new)
+// 0x7875        Info-ZIP UNIX (newer UID/GID)
+// 0xa11e        Data Stream Alignment (Apache Commons-Compress)
+// 0xa220        Microsoft Open Packaging Growth Hint
+// 0xcafe        Java JAR file Extra Field Header ID
+// 0xd935        Android ZIP Alignment Extra Field
+// 0xe57a        Korean ZIP code page info
+// 0xfd4a        SMS/QDOS
+// 0x9901        AE-x encryption structure (see APPENDIX E)
+// 0x9902        unknown
+
+constexpr int zip64ExtraID = 0x0001;                     // Zip64 extended information
+constexpr int ntfsExtraID = 0x000a;                      // NTFS
+constexpr int unixExtraID = 0x000d;                      // UNIX
+constexpr int extTimeExtraID = 0x5455;                   // Extended timestamp
+constexpr int infoZipUnixExtraID = 0x5855;               // Info-ZIP Unix extension
+constexpr int experimentalXlID = 0x6c78;                 // Experimental 'xl' field
+constexpr int infoZipUnicodeCommentExtraID = 0x6375;     // Info-ZIP Unicode Comment Extra Field.
+constexpr int infoZipUnicodePathID = 0x7075;             // Info-ZIP Unicode Path Extra Field.
+constexpr int infoZipUnixExtra2ID = 0x7855;              // Info-ZIP Unix Extra Field (type 2) "Ux". *
+constexpr int infoZipUnixExtra3ID = 0x7875;              // Info-Zip Unix Extra Field (type 3) "ux". *
+constexpr int winzipAesExtraID = 0x9901;                 // winzip AES Extra Field
+constexpr int msOpenPackagingGrowthHintExtraID = 0xa220; // Microsoft Open Packaging Growth Hint
+constexpr int apkAlignExtraID = 0xD935;                  // Android ZIP Alignment Extra Field
+constexpr int javaJARExtraID = 0xcafe;                   // Java JAR file Extra Field Header ID
 
 constexpr auto s_IFMT = 0xf000;
 constexpr auto s_IFSOCK = 0xc000;
