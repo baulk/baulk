a2dcb432905698f37d9452c507f35e05af4bc9f0
Force Charlie <charlieio@outlook.com>
2023-12-31 18:31:48 +0800

    Sync bela

--- vendor/bela/src/belahash/blake3/blake3_dispatch.c
+++ vendor/bela/src/belahash/blake3/blake3_dispatch.c
@@ -32,9 +32,9 @@
 #define ATOMIC_LOAD(x) x
 #define ATOMIC_STORE(x, y) x = y
 #elif defined(_MSC_VER)
-#define ATOMIC_INT long
-#define ATOMIC_LOAD(x) _InterlockedOr(&x, 0)
-#define ATOMIC_STORE(x, y) _InterlockedExchange(&x, y)
+#define ATOMIC_INT LONG
+#define ATOMIC_LOAD(x) InterlockedOr(&x, 0)
+#define ATOMIC_STORE(x, y) InterlockedExchange(&x, y)
 #else
 #define ATOMIC_INT int
 #define ATOMIC_LOAD(x) x
