ec749ddae21d08a2a80cc5d79d14494288df0597
J23 <charlieio@outlook.com>
2025-02-21 20:52:48 +0800

    Copyright © 2025

--- vendor/bela/src/belatime/time.cc
+++ vendor/bela/src/belatime/time.cc
@@ -1,5 +1,5 @@
 // ---------------------------------------------------------------------------
-// Copyright © 2024, Bela contributors
+// Copyright © 2025, Bela contributors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
